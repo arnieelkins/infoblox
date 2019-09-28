a9f98cd96d4e161a297d96ec99cf27ee15da2fa6
Arnie Elkins <arnie.elkins@gmail.com>
2019-09-28 17:59:57 -0500

    I am thinking it should work but I can only test on http://httpbin.org

--- infoblox.py
+++ infoblox.py
@@ -9,19 +9,20 @@ import requests
 
 
 def create_host_record_next_ip(hostname, subnet):
-    #base_url = "http://INFOBLOX_IP"
-    #endpoint = "wapi/v1.2/record:host"
+    # base_url = "http://172.16.15.254"
+    # endpoint = "wapi/v1.2/record:host"
     base_url = "http://httpbin.org"
     endpoint = "/post"
     url = base_url + endpoint
-    headers = {"Content-Type": "application/json"}
+    header_dict = {"Content-Type": "application/json"}
     func = "func:nextavailableip:{}".format(subnet)
     data = {"name": hostname, "ipv4addrs": [{"ipv4addr": func}]}
 
-    reply = requests.post(url, headers, data)
+    reply = requests.post(url, headers=header_dict, json=data)
     return reply
 
 
 if __name__ == "__main__":
-    output = create_host_record_next_ip("my_new_hostname", "192.168.100.0/24")
-    print(output.json())
+    output = create_host_record_next_ip("my_new_host", "192.168.100.0/24")
+    for section in output:
+        print(section)
